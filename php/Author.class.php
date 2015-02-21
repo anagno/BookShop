c804910e34b389f35932a1ae88060c9d41da3718
Vasilis-Thanos Anagnostopoulos <anagnwstopoulos@hotmail.com>
2015-02-21 16:52:33 +0200

    Adding the class Author

---
+++ php/Author.class.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once "DataObject.class.php";
+
+/**
+ * Defining the name of the table that is used to store the users
+ */
+define('TABLE_AUTHOR', 'authors');
+
+class Author extends DataObject 
+{
+	public static function get($id) 
+	{
+		$conn = parent::connect();
+		$sql = 'SELECT * FROM ' . TABLE_AUTHOR . ' WHERE id = :id';
+		try 
+		{
+			$st = $conn-> prepare( $sql );
+			$st-> bindValue( ":id", $id, PDO::PARAM_INT );
+			$st-> execute();
+			$row = $st-> fetch();
+			parent::disconnect( $conn );
+			
+			if ( $row ) 
+				return new Author( $row );
+		} 
+		catch ( PDOException $e ) 
+		{
+			parent::disconnect( $conn );
+			die( "Query failed: " . $e->getMessage() );
+		}
+	}
+	
+	public static function add($name)
+	{					
+		$conn = parent::connect();
+		$sql = 'INSERT INTO ' . TABLE_AUTHOR . ' (name) VALUES(:name)';
+		try
+		{
+			$st = $conn-> prepare( $sql );
+			$st-> bindValue( ":name", $name, PDO::PARAM_STR );
+			$st-> execute();
+			parent::disconnect( $conn );
+
+		}
+		catch ( PDOException $e )
+		{
+			parent::disconnect( $conn );
+			die( "Query failed: " . $e->getMessage() );
+		}
+	}
+	
+	public static function delete($id)
+	{
+		$conn = parent::connect();
+		$sql = 'DELETE FROM ' . TABLE_AUTHOR . ' WHERE id = :id';
+		try
+		{
+			$st = $conn-> prepare( $sql );
+			$st-> bindValue( ":id", $id, PDO::PARAM_INT );
+			$st-> execute();
+			parent::disconnect( $conn );
+	
+		}
+		catch ( PDOException $e )
+		{
+			parent::disconnect( $conn );
+			die( "Query failed: " . $e->getMessage() );
+		}
+	}	
+	
+	protected $data = array(
+			"id" => "",
+			"name" => "",
+	);
+	
+}
+
+?>
---
+++ public_html/AuthorView.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "../php/Author.class.php";
+
+if ( isset( $_GET["author_id"] ) )
+	$author_id = (int) $_GET["author_id"];
+
+if(!$author = Author::get($author_id))
+{
+	die("Error: Author not found");
+}
+
+?>
+
+<!-- http://www.w3schools.com/tags/tag_dl.asp -->
+
+<dl>
+
+<dt> Author id </dt> <dd> <?php echo $author-> getValueEncoded( "id" ) ?> </dd>
+<dt> Name </dt> <dd> <?php echo $author-> getValueEncoded( "name" ) ?> </dd>
+
+</dl>
+
+
+
+
